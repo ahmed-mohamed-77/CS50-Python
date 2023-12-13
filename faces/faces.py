aa0c7e4b114b53c4bd9ae6a99e816106fae34bb3
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-12-13 02:47:59 +0000

    Wed, Dec 13, 2023, 4:47 AM +02:00

--- faces/faces.py
+++ faces/faces.py
@@ -1,4 +1,8 @@
 input_var = input("enter a sentence: ")
-input_var = input_var.captalize()
-
-print(input_var)
+input_var = input_var.capitalize()
+if input_var == "Hello :)":
+    print(f"{input_var} 😊")
+elif input_var == "Goodbye :(":
+    print(f"{input_var} 😒")
+if input_var == "Hello :)":
+    print(f"{input_var} 😊")
