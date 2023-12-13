4509f2c5c8d5d42d45fb970a139d3860544c8ece
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-12-13 02:52:50 +0000

    Wed, Dec 13, 2023, 4:52 AM +02:00

--- faces/faces.py
+++ faces/faces.py
@@ -5,5 +5,5 @@ if "Hello :)" in input_var:
     print("Hello 🙂")
 elif "Goodbye :(" in input_var:
     print("Goodbye 🙁")
-elif "Hello :)" in input_var and "Goodbye :(" in input_var:
+elif "Hello :)" in input_var and " Goodbye :(" in input_var:
     print("Hello 🙂 Goodbye 🙁")
