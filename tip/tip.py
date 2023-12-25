3805674473fa75fd8b5277204d3f3108783c2183
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-12-25 16:11:58 +0000

    Mon, Dec 25, 2023, 6:11 PM +02:00

--- tip/tip.py
+++ tip/tip.py
@@ -1,16 +1,16 @@
 def main():
-    dollars = dollars_to_float(input("How much was the meal? "))
-    percent = percent_to_float(input("What percentage would you like to tip? "))
+    dollars = dollars_to_float()
+    percent = percent_to_float()
     tip = dollars * percent
     print(f"Leave ${tip:.2f}")
 
 
-def dollars_to_float(d):
-    # TODO
+def dollars_to_float(doller):
+    doller = float(input("How much was the meal? "))
+    return doller
 
-
-def percent_to_float(p):
-    # TODO
+def percent_to_float(percentage):
+    percentage = float(input("What percentage would you like to tip? "))
 
 
 main()
