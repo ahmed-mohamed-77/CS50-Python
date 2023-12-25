10d47999e5f0a57964057ea715ccc9bda1532336
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-12-25 17:22:52 +0000

    Mon, Dec 25, 2023, 7:22 PM +02:00

--- einstein/einstein.py
+++ einstein/einstein.py
@@ -1,7 +1,11 @@
-m = float(input("m: "))
-c = 300000000
-result = m * (c ** 2)
+def main():
+    result = Mass_energy_equivalence()
+    formatted_result = "{:f}".format(result)
+    print("E:", formatted_result)
 
-formatted_result = "{:f}".format(result)
-print("E:", formatted_result)
+def Mass_energy_equivalence():
+    m = float(input("m: "))
+    c = 300000000
+    return m * (c ** 2)
 
+main()
