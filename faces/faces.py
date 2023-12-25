d81076235c0635f73b2ff7a2b42a8d98379467f5
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-12-25 15:32:12 +0000

    Mon, Dec 25, 2023, 5:32 PM +02:00

--- faces/faces.py
+++ faces/faces.py
@@ -9,9 +9,9 @@ def main():
 def convert(msg):
     # convert :) to happy emoji
     msg1 = msg.replace(":)", "🙂")
+    # convert :) to sad emoji
     msg2 = msg1.replace(":(", "🙁")
     return msg2
-
 
 main()
 
