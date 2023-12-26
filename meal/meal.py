b5ed556a998825fe7c898058cebdf4bbaa4453c8
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-12-26 16:11:19 +0000

    Tue, Dec 26, 2023, 6:11 PM +02:00

--- meal/meal.py
+++ meal/meal.py
@@ -7,14 +7,14 @@ def main():
 
 def convert(time):
 
-     hour, min = [int(i) for i in time.split(":")]
+    hour, min = [int(i) for i in time.split(":")]
 
-     if (7 >= hour <= 8) and (0 >= min <= 60):
+    if (7 >= hour <= 8) and (0 >= min <= 60):
         print("breakfast time")
-     elif (7 >= hour <= 8) and (0 >= min <= 60):
-        print("lunch time")
-     elif (7 >= hour <= 8) and (0 >= min <= 60):
-        print("dinner time")
+    elif 12.0 <= time_in_hours <= 13.0:
+        print("It's lunch time!")
+    elif 18.0 <= time_in_hours <= 19.0:
+        print("It's dinner time!")
 
 if __name__ == "__main__":
     main()
