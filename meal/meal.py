c1f0e79bbc9c040f7157a88e91a47c9a6df1dfa3
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-12-26 15:51:47 +0000

    Tue, Dec 26, 2023, 5:51 PM +02:00

--- meal/meal.py
+++ meal/meal.py
@@ -1,10 +1,14 @@
 
 
 def main():
+meal_time = input("Enter times in HH:MM\n").split()
 
+for time in alltimes:
+     hour, min = [int(i) for i in time.split(":")]
+     print(hour, "hours and", min, "minutes")
 
 def convert(time):
-    if time <= 
+    if time <=
 
 if __name__ == "__main__":
     main()
