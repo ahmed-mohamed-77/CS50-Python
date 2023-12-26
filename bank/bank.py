5cfa911810e45d4bf5fc3d083af7211327e108b3
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-12-26 19:08:08 +0000

    Tue, Dec 26, 2023, 9:08 PM +02:00

--- bank/bank.py
+++ bank/bank.py
@@ -1,7 +1,8 @@
 # greeting from the teller
-greeting = input("Greeting: ").captilize()
+greeting = input("Greeting: ").capitalize()
 # hello pays $0
-if
+if "Hello" in greeting:
+    print("$0")
 # any greeting starting with H pays $20
-
+elif
 # any other greetings
