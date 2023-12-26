b51f05c7ca6ebd4747bba521657ded1becc923a6
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-12-26 18:12:36 +0000

    Tue, Dec 26, 2023, 8:12 PM +02:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -1,11 +1,11 @@
 def main()
-.gif
-.jpg
-.jpeg
-.png
-.pdf
-.txt
-.zip
+.gif image/gif
+.jpg image/jpeg
+.jpeg image/jpeg
+.png image/png
+.pdf application/pdf
+.txt text/plain
+.zip application/zip
 
 if__name__ == "__main__":
     main()
