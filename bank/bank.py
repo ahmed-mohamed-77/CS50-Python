66da63417b60bae834cc0ff67067fff0f8d25741
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-12-26 19:10:48 +0000

    Tue, Dec 26, 2023, 9:10 PM +02:00

--- bank/bank.py
+++ bank/bank.py
@@ -4,5 +4,10 @@ greeting = input("Greeting: ").capitalize()
 if "Hello" in greeting:
     print("$0")
 # any greeting starting with H pays $20
-elif
+elif ["How you doing", "Hey", "How's it going"] in greeting:
+    print("$20")
+# elif "Hey"
+# elif "How's it going"
 # any other greetings
+else:
+    print("$100")
