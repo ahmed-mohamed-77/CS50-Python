a7af0bdf1d6f9c163386f79ffcf485b80b92b098
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-12-26 16:00:02 +0000

    Tue, Dec 26, 2023, 6:00 PM +02:00

--- meal/meal.py
+++ meal/meal.py
@@ -9,7 +9,8 @@ def convert(time):
     for time in alltimes:
      hour, min = [int(i) for i in time.split(":")]
 
-     if 
+     if (7 >= hour <= 8) and (0 >= min <= 60):
+        print("breakfast time")
 
 if __name__ == "__main__":
     main()
