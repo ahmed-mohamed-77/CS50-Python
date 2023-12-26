8b2a4f71ad735cc12b26a3136ce2ffec64a63ada
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-12-26 16:27:17 +0000

    Tue, Dec 26, 2023, 6:27 PM +02:00

--- interpreter/interpreter.py
+++ interpreter/interpreter.py
@@ -1,7 +1,9 @@
 def main():
-    x,y,z = input("Enter a calculations" ). split(" ")
+    x,y,z = input("Enter a calculations: "). split(" ")
     print (x, y, z)
-    
+    print(type(x))
+    print(type(y))
+    print(type(z))
 
 
 if __name__ == "__main__":
