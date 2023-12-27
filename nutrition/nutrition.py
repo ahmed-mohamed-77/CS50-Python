7abdf1c17a4df5c0281443c93672ae33243b93fc
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-12-27 16:16:46 +0000

    Wed, Dec 27, 2023, 6:16 PM +02:00

--- nutrition/nutrition.py
+++ nutrition/nutrition.py
@@ -1,8 +1,14 @@
 def main():
+    user_input = input("Item: ").strip().lower()
 
-fruits = {
-    
-}
+    fruits = {
+        "apple" : 130, "Avocade" : 50, "Banana" : 110, "Cantaloupe" : 50,
+        "Grapefruit" : 60, "Grapes" : 90, "honeydew Melon" : 50,
+        "Kiwifruit" : 90, "Lemon" : 15, "Lime" : 20, "Nectarine" : 60,
+        "Orange" : 80, "Peach" : 60, "Pear" : 100, "Pineapple" : 50,
+        "Plims" : 70, "Strawberries" : 50, "Sweet Cherries" : 100,
+        "Tangerine" : 50, "Watermelon" : 80
+    }
 
 if __name__ == "__main__":
     main()
