ddecdaace14478f394b4e5636af81fb1d46df1c7
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-12-28 14:34:16 +0000

    Thu, Dec 28, 2023, 4:34 PM +02:00

--- twttr/twttr.py
+++ twttr/twttr.py
@@ -1,6 +1,8 @@
 user_input = input("Input: ").strip()
 user_output = ""
+
 for c in user_input:
     if c not in ["A", "E", "I", "O", "U"]:
-        user_output += char
-    print(c, end="")
+        user_output += c
+
+print(user_output, end="")
