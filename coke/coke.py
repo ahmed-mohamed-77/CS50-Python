eb9037a3cf704e5bfc49f26fa3296ff449f925c8
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-12-28 14:56:32 +0000

    Thu, Dec 28, 2023, 4:56 PM +02:00

--- coke/coke.py
+++ coke/coke.py
@@ -3,8 +3,9 @@ coke_price = 50
 due_coin = [25, 10, 5]
 
 while coke_price >= 0:
-    if due_coin in customer:
-        customer = int(input("Insert Coin: "))
-        coke_price -= customer
-    else:
-        print("the machine accept only [25 ,10 ,5]")
+    for coin in customer:
+        if due_coin in coin:
+            coke_price -= customer
+            customer = int(input("Insert Coin: "))
+        else:
+            print("the machine accept only [25 ,10 ,5]")
