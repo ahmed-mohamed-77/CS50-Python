b171ff37e48e74b9458d5296dcf952c66c9c16d1
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-12-28 15:08:30 +0000

    Thu, Dec 28, 2023, 5:08 PM +02:00

--- coke/coke.py
+++ coke/coke.py
@@ -5,9 +5,12 @@ print(f"Amount Due: {coke_price}")
 while coke_price > 0:
     customer_coin = int(input("Insert Coin: "))
 
-    if customer_coin in due_coins:
+    if customer_coin not in due_coin:
+        print(f"Amount Due: {coke_price}")
+    elif customer_coin in due_coins:
         coke_price -= customer_coin
         print(f"Amount Due: {coke_price}")
+        
 print(f"Change Owed: {coke_price}")
 
 
