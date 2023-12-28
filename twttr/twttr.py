fb5a7495ee1842280f851e5ff7228f4eaf8a6596
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-12-28 14:40:15 +0000

    Thu, Dec 28, 2023, 4:40 PM +02:00

--- twttr/twttr.py
+++ twttr/twttr.py
@@ -1,9 +1,10 @@
 user_input = input("Input: ").strip()
 user_output = ""
-
+vowels_lower = ["a", "e", "i", "o", "u"]
+vowels_upper = ["A", "E", "I", "O", "U"]
 
 for c in user_input:
-    if c not in ["a", "e", "i", "o", "u"]:
+    if c not in vowels_lower or c not in vowels_upper:
         user_output += c
 
 print(f"output: {user_output}")
