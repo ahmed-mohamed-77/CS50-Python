dd813ca4d674ed12a3e75fd98ec2ab3105c83201
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-12-28 14:53:15 +0000

    Thu, Dec 28, 2023, 4:53 PM +02:00

--- coke/coke.py
+++ coke/coke.py
@@ -3,4 +3,7 @@ coke_price = 50
 due_coin = [25, 10, 5]
 
 while coke_price >= 0:
-    
+    if due_coin in customer:
+        coke_price -= customer
+    else:
+        print("the machine accept only [25 ,10 ,5])
