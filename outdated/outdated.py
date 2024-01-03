744489b235a8b618fa64f30b1298c8d3c64c7855
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-01-03 02:20:40 +0000

    Wed, Jan 3, 2024, 4:20 AM +02:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -1,23 +1,40 @@
 import datetime
 
-# Get the input date from the user
-outdated = input("Date: ")
+month = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+]
 
-# Check if the input date contains slashes (MM/DD/YYYY format)
-if "/" in outdated:
-    # If slashes are present, split the input using slashes
-    month_out, day_out, year_out = map(int, outdated.split("/"))
+try:
+    # Get the input date from the user
+    outdated = input("Date: ")
 
-    # Create a datetime object with the parsed values
-    updated_date = datetime.datetime(year_out, month_out, day_out)
+    # Check if the input date contains slashes (MM/DD/YYYY format)
+    if "/" in outdated:
+        # If slashes are present, split the input using slashes
+        month_out, day_out, year_out = map(int, outdated.split("/"))
 
-    # Print the formatted date
-    print(updated_date.strftime("%Y-%m-%d"))
+        # Create a datetime object with the parsed values
+        updated_date = datetime.datetime(year_out, month_out, day_out)
 
-# If no slashes are present, assume the input date is in "Month Day, Year" format
-else:
-    # Parse the input date using strptime with the format "%B %d, %Y"
-    input_date = datetime.datetime.strptime(outdated, "%B %d, %Y")
+        # Print the formatted date
+        print(updated_date.strftime("%Y-%m-%d"))
 
-    # Print the formatted date
-    print(input_date.strftime("%Y-%m-%d"))
+    # If no slashes are present, assume the input date is in "Month Day, Year" format
+    else:
+        # Parse the input date using strptime with the format "%B %d, %Y"
+        input_date = datetime.datetime.strptime(outdated, "%B %d, %Y")
+
+        # Print the formatted date
+        print(input_date.strftime("%Y-%m-%d"))
+except:
