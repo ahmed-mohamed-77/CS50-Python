0c7a8ee61cfbebf918fd76141dc6c319da0e7100
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-01-04 14:04:45 +0000

    Thu, Jan 4, 2024, 4:04 PM +02:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -19,7 +19,7 @@ while True:
             # Attempt to parse the input with full month name (Month Day, Year format)
             old_month, old_day, year = outdated.split(" ")
             # Find the position of the month in the list
-            for i in range(len(old_month)):
+            for i in range(len(months)):
                 if old_month == months[i]:
                     month = i + 1
 
