3d3408f87ed52cdc56310a58710aa011932b5a7f
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-01-04 10:10:09 +0000

    Thu, Jan 4, 2024, 12:10 PM +02:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -6,7 +6,7 @@ import datetime
 user_date = input("Date: ").strip()
 
 # Define multiple format strings for parsing
-formats = ["%B %d, %Y", "%B %d %Y", "%d %B %Y", "%m/%d/%Y", "%d/%m/%Y", "%B/%d/%Y"]
+formats = ["%B %d, %Y", "%B %d %Y", "%d %B %Y", "%m/%d/%Y"]
 parse_date = None
 for fmt in formats:
   try:
@@ -19,16 +19,3 @@ if parse_date:
   unified_date = "%Y-%m-%d"
   formatted_date = parse_date.strftime(unified_date)
   print(formatted_date)
-
-:( input of 23/6/1912 results in reprompt
-    expected program to reject input, but it did not
-:( input of 10 December, 1815 results in reprompt
-    expected program to reject input, but it did not
-:( input of October/9/1701 results in reprompt
-    expected program to reject input, but it did not
-:( input of 1/50/2000 results in reprompt
-    expected program to reject input, but it did not
-:( input of December 80, 1980 results in reprompt
-    expected program to reject input, but it did not
-:( input of September 8 1636 results in reprompt
-    expected program to reject input, but it did not
