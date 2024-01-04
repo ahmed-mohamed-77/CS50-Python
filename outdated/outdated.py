455a9d416c57a690a748f489b44ae35d55bd3279
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-01-04 14:01:15 +0000

    Thu, Jan 4, 2024, 4:01 PM +02:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -33,3 +33,19 @@ while True:
 
 # Print the formatted date
 print(f"{year}-{int(month):02d}-{int(day):02d}")
+
+
+Date: December 80, 1980
+Traceback (most recent call last):
+  File "/home/ubuntu/CS50-Python/outdated/outdated.py", line 12, in <module>
+    month, day, year = map(int, outdated.split("/"))
+    ^^^^^^^^^^^^^^^^
+ValueError: invalid literal for int() with base 10: 'December 80, 1980'
+
+During handling of the above exception, another exception occurred:
+
+Traceback (most recent call last):
+  File "/home/ubuntu/CS50-Python/outdated/outdated.py", line 26, in <module>
+    if (1 <= int(month) <= 12) and (1 <= int(day) <= 31):
+                 ^^^^^
+NameError: name 'month' is not defined. Did you mean: 'months'?
