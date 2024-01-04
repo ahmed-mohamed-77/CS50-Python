f29e8802b0329b8b6a1337a09d83b0e600a04555
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-01-04 15:14:29 +0000

    Thu, Jan 4, 2024, 5:14 PM +02:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -1,3 +1,17 @@
-#  percentage=(denominator / numerator)×100
-user_input = input("Fraction: ")
-numerator, denominator = map(int, user_input.split("/"))
+while True:
+  try:
+    #  percentage=(denominator / numerator)×100
+    user_input = input("Fraction: ")
+    numerator, denominator = map(int, user_input.split("/"))
+    if numerator > denominator:
+      raise ValueError
+    if denominator == 4:
+      denominator = denominator
+    else:
+      raise ValueError
+
+    print(f"%{(numerator/denominator) * 100}")
+    break
+  except (ValueError, ZeroDivisionError):
+    print()
+    pass
