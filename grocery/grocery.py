3d02f5256aa7c4b66d51f516c466317f80c3c534
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-01-04 16:58:25 +0000

    Thu, Jan 4, 2024, 6:58 PM +02:00

--- grocery/grocery.py
+++ grocery/grocery.py
@@ -1,8 +1,12 @@
-try:
-    items = []
-    item = input()
-    items.append(item)
-    for i in range(len(items)):
-        
-except EOFError:
-    ...
+while True:
+    try:
+        items = []
+        item = input()
+        items.append(item)
+
+    except EOFError:
+        print()
+        pass
+    else:
+        for i in range(len(items)):
+            print((i + 1), items[i])
