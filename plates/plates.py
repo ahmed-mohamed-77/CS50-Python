be8c98221b0193212364be0d012bdcc421fa8648
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-01-15 02:51:57 +0000

    Mon, Jan 15, 2024, 4:51 AM +02:00

--- plates/plates.py
+++ plates/plates.py
@@ -5,23 +5,9 @@ def main():
     else:
         print("Invalid")
 
-def is_valid(plate_number):
-    # Check if plate number has less than 2 or more than 6 characters
-    if len(plate_number) < 2 or len(plate_number) > 6:
-        return False
 
-    # Check if plate number starts with at least two letters
-    if not plate_number[:2].isalpha():
-        return False
+def is_valid(s):
+    ...
 
-    # Check if numbers are used in the middle of the plate
-    if any(char.isdigit() for char in plate_number[2:-1]):
-        return False
-
-    # Check if the first number used is '0'
-    if plate_number[-1].isdigit() and plate_number[-1] == '0':
-        return False
-
-    return True
-
-main()
+if __name__ == "__main__":
+    main()
