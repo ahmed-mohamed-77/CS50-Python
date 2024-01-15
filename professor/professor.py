c19145abbabcc42236a46e1551e4592509be31fd
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-01-15 01:11:59 +0000

    Mon, Jan 15, 2024, 3:11 AM +02:00

--- professor/professor.py
+++ professor/professor.py
@@ -1,27 +1,31 @@
 import random
+import sys
+
 def main():
-    while True:
-        # Get the user's chosen level
-        user = get_level()
-        if 0 < user <= 3:
-        # Loop for 10 questions
-            for i in range(10):
-                try:
-                    # Generate random integers based on the user's level
-                    x, y = generate_integer(user)
+    # Get the user's chosen level
+    user = get_level()
 
-                    # Prompt the user for an answer
-                    user_answer = int(input(f"{x} + {y} = "))
+    # Initialize the counter for correct answers
+    counter = 0
 
-                    # Check if the user's answer is correct
-                    if user_answer == (x + y):
-                        counter = i + 1  # Increment counter for correct answers
-                    else:
-                        print("EEE")  # Print error message for incorrect answers
-                except ValueError:
-                    pass  # Ignore ValueError (non-integer input)
-            if 0 < user <= 3:
-                break
+    # Loop for 10 questions
+    for i in range(10):
+        try:
+            # Generate random integers based on the user's level
+            x, y = generate_integer(user)
+
+            # Prompt the user for an answer
+            user_answer = int(input(f"{x} + {y} = "))
+
+            # Check if the user's answer is correct
+            if user_answer == (x + y):
+                counter += 1  # Increment counter for correct answers
+            else:
+                print("EEE")  # Print error message for incorrect answers
+
+        except ValueError:
+            pass  # Ignore ValueError (non-integer input)
+
     # Print the user's final score
     print(f"Score: {counter}")
 
@@ -34,16 +38,17 @@ def get_level():
 def generate_integer(level):
     # Generate random integers based on the user's chosen level
     if level == 1:
-        x = random.randint(0, 9)
-        y = random.randint(0, 9)
+        x = random.randint(1, 10)
+        y = random.randint(1, 10)
     elif level == 2:
-        x = random.randint(10, 999)
-        y = random.randint(10, 999)
+        x = random.randint(1, 100)
+        y = random.randint(1, 100)
     elif level == 3:
-        x = random.randint(10, 999)
-        y = random.randint(10, 999)
+        x = random.randint(1, 1000)
+        y = random.randint(1, 1000)
+    else:
+        sys.exit()  # Exit the program for an invalid level
     return x, y
-
 
 
 if __name__ == "__main__":
