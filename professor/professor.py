71e2579cbbac3923001068fad1da186764d757a0
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-01-15 01:29:32 +0000

    Mon, Jan 15, 2024, 3:29 AM +02:00

--- professor/professor.py
+++ professor/professor.py
@@ -1,29 +1,32 @@
 import random
 def main():
-    while True:
-        # Get the user's chosen level
-        user = get_level()
-        error_counter = 0
-        if 0 < user <= 3:
-        # Loop for 10 questions
-            for i in range(10):
-                try:
-                    # Generate random integers based on the user's level
-                    x, y = generate_integer(user)
 
-                    # Prompt the user for an answer
-                    user_answer = int(input(f"{x} + {y} = "))
+    # Get the user's chosen level
+    user = get_level()
+    error_counter = 0
+    if not (0 < user <= 3):
+        raise ValueError
+    # Loop for 10 questions
+    for i in range(10):
+        try:
+            # Generate random integers based on the user's level
+            x, y = generate_integer(user)
 
-                    # Check if the user's answer is correct
-                    if user_answer == (x + y):
-                        counter = i + 1  # Increment counter for correct answers
-                    else:
-                        print("EEE")  # Print error message for incorrect answers
-                        if err
-                except ValueError:
-                    pass  # Ignore ValueError (non-integer input)
-            if 0 < user <= 3:
-                break
+            # Prompt the user for an answer
+            user_answer = int(input(f"{x} + {y} = "))
+
+            # Check if the user's answer is correct
+            if user_answer == (x + y):
+                counter = i + 1  # Increment counter for correct answers
+            else:
+                print("EEE")  # Print error message for incorrect answers
+                error_counter += 1
+                if error_counter == 3:
+                    print(f"{x} + {y} = {x + y}")
+                    error_counter = 0
+        except ValueError:
+            pass  # Ignore ValueError (non-integer input)
+
     # Print the user's final score
     print(f"Score: {counter}")
 
