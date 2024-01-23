2ce015e94e4d892936ddbf1e0b3a80e4de9ca328
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-01-23 14:14:46 +0000

    Tue, Jan 23, 2024, 4:14 PM +02:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -1,29 +1,37 @@
 def main():
     while True:
-      try:
-          user = input("Fraction: ").strip()
+        try:
+            # Prompt the user for input
+            user = input("Fraction: ").strip()
 
-          percentage = convert(user)
-          gauge(percentage)
-          break
+            # Convert the user input to percentage
+            percentage = convert(user)
 
-      except (ValueError, ZeroDivisionError):
-          ...
+            # Display the result using the gauge function
+            gauge(percentage)
 
+            # Break out of the loop if no exception occurs
+            break
 
+        except (ValueError, ZeroDivisionError):
+            # Handle invalid input, print error message, and continue the loop
+            pass
 
 def convert(fraction):
+    # Convert the user input fraction to integers
     number_one, number_two = map(int, fraction.split("/"))
 
+    # Check if the numerator is greater than the denominator
     if number_one > number_two:
         raise ValueError
 
+    # Calculate the percentage and round it
     result = round((number_one / number_two) * 100)
     return result
 
 
-
 def gauge(percentage):
+    # Display the gauge result based on the calculated percentage
     if percentage >= 99:
         print("F")
     elif percentage <= 1:
