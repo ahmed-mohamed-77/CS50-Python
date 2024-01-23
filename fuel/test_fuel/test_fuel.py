a79bffa8544d862ba373b5e4d56c7da986ef6a95
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-01-23 15:53:50 +0000

    Tue, Jan 23, 2024, 5:53 PM +02:00

--- fuel/test_fuel/test_fuel.py
+++ fuel/test_fuel/test_fuel.py
@@ -1,11 +1,22 @@
-from fuel.fuel import gauge, convert
+from fuel import convert, gauge
+import pytest
+
+# zero division
+def test_zero_division():
+  with pytest.raises(ZeroDivisionError):
+    assert convert("1/0")
+
+# value error
+def test_value():
+  with pytest.raises(ValueError):
+    assert convert("cat/dog")
 
 def test_convert():
   assert convert("0/1") == 0
   assert convert("3/4") == 75
-  assert convert("4/4") == "1"
+  assert convert("4/4") == 1
 
 def test_gauge():
-  assert gauge("4/4") == "F"
-  assert gauge("0/1") == "E"
+  assert gauge(99) == "F"
+  assert gauge(0) == "E"
   assert gauge("3/4") == "75%"
