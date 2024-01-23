13310a2bfe0ad8a38c5994f6c69ed8898ab7a7bc
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-01-23 13:58:25 +0000

    Tue, Jan 23, 2024, 3:58 PM +02:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -1,32 +1,3 @@
-# while True:
-#     try:
-#         # Get the user input as a fraction
-#         user_input = input("Fraction: ")
-
-#         # Split the input into numerator and denominator
-#         numerator, denominator = map(int, user_input.split("/"))
-
-#         # Check if the numerator is greater than the denominator
-#         if numerator > denominator:
-#             raise ValueError
-
-#         # Calculate the percentage and round to the nearest integer
-#         result = round((numerator / denominator) * 100)
-
-#         # Evaluate the percentage and print the corresponding grade
-#         if result <= 1:
-#             print("E")
-#         elif result >= 99:
-#             print("F")
-#         else:
-#             print(f"{result}%")
-
-#         break  # Exit the loop if input is valid
-
-#     except (ValueError, ZeroDivisionError):
-#         print()
-#         pass  # Continue to the next iteration if an exception is caught
-
 def main():
     while True:
       try:
