c0d8ae758f713894c5c29ec04e4db4b80e931051
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-01-25 06:56:03 +0000

    Thu, Jan 25, 2024, 8:56 AM +02:00

--- plates/plates.py
+++ plates/plates.py
@@ -6,8 +6,32 @@ def main():
         print("Invalid")
 
 
-def is_valid(s):
-    ...
+def is_valid(plate_number):
+    # check for the  length palte_number
+    # max == 6
+    # min == 2
+    if 2 < len(plate_number) <= 6:
+        return True
+
+    if plate_number[0].isalpha() == False or  plate_number[1].isalpha() == False:
+        return False
+    i = 0
+
+    while i < len(plate_number) :
+    
+        if plate_number[i].isalpha() == False:
+            if plate_number[i] == "0":
+                return False
+            else:
+                break
+        i += 1
+
+    for character in plate_number:
+        if character in [".", " ", "!", "?"]:
+            return False
+    
+    return True
+
 
 if __name__ == "__main__":
     main()
