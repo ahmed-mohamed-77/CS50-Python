76eb8a15b565f749ff96f4948ad64a6a40e3b37e
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-01-26 18:12:04 +0000

    Fri, Jan 26, 2024, 8:12 PM +02:00

--- plates/plates.py
+++ plates/plates.py
@@ -1,56 +1,35 @@
+import sys
+
 def main():
     plate = input("Plate: ")
     if is_valid(plate):
         print("Valid")
+        sys.exit(0)  # Exit with code 0 for success
     else:
         print("Invalid")
-
+        sys.exit(1)  # Exit with a non-zero code for failure
 
 def is_valid(s):
-    if check_length(s) and check_begin(s) and check_spec(s) and check_numbers(s):
-        return True
-    else:
-        return False
-
+    return check_length(s) and check_begin(s) and check_spec(s) and check_numbers(s)
 
 def check_length(s):
-    if len(s) < 2 or len(s) > 6:
-        return False
-    else:
-        return True
-
+    return 2 <= len(s) <= 6
 
 def check_begin(s):
-    for c in s[:2]:
-        if c.isdigit():
-            return False
-    return True
-
+    return not any(c.isdigit() for c in s[:2])
 
 def check_spec(s):
-    for c in s:
-        if not c.isalpha() and not c.isdigit():
-            return False
-    return True
-
+    return all(c.isalpha() or c.isdigit() for c in s)
 
 def check_numbers(s):
-    firstnum = None
-    for c in s:
-        if c.isdigit():
-            firstnum = c
-            break
-    if firstnum == None:
+    firstnum = next((c for c in s if c.isdigit()), None)
+    if firstnum is None:
         return True
     if int(firstnum) == 0:
         return False
     index = s.index(firstnum)
     position = len(s) - index
-    for c in s[-position:]:
-        if not c.isdigit():
-            return False
-    return True
-
+    return all(c.isdigit() for c in s[-position:])
 
 if __name__ == "__main__":
     main()
