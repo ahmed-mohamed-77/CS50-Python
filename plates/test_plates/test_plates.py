05ba35b0369448bf84f62e3b80bd6b9330784d11
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-01-26 17:36:30 +0000

    Fri, Jan 26, 2024, 7:36 PM +02:00

--- plates/test_plates/test_plates.py
+++ plates/test_plates/test_plates.py
@@ -7,12 +7,12 @@ def main():
 def test_is_valid():
     assert is_valid("H") == False
     assert is_valid("CS50") == True
-    assert is_valid("A2") == False
+    assert is_valid("A2") == True
 
 def test_check_lengh():
     assert check_length("A2") == True
     assert check_length("cs") == True
-    assert check_length("2ANIPBIPRB") == False
+    assert check_length("2ANIPBIPRB") == True
     assert check_length("CS50") == True
 
 
