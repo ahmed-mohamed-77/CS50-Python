5c5de60eb93ef7a2f840f4251d3bf65d2ec41f67
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-01-26 18:04:45 +0000

    Fri, Jan 26, 2024, 8:04 PM +02:00

--- plates/test_plates/test_plates.py
+++ plates/test_plates/test_plates.py
@@ -3,17 +3,36 @@ from plates.plates import *
 def main():
     test_is_valid()
     test_check_lengh()
+    min_max_character()
+    test_num_zero()
+    test_punctuation()
 
 def test_is_valid():
     assert is_valid("H") == False
     assert is_valid("CS50") == True
-    assert is_valid("A2") == True
+    assert is_valid("ABCDEF") == True
+    assert is_valid("ABCDEFGH") == False
+    assert is_valid("A") == False
+
 
 def test_check_lengh():
-    assert check_length("A2") == True
-    assert check_length("cs") == True
-    assert check_length("2ANIPBIPRB") == True
-    assert check_length("CS50") == True
+    assert is_valid("A2") == False
+    assert is_valid("AA") == True
+    assert is_valid("2A") == False
+    assert is_valid("22") == False
+
+def min_max_character():
+    assert is_valid("AAA222") == True
+    assert is_valid("AAA22A") == False
+
+def test_num_zero():
+    assert is_valid("CS50") == True
+    assert is_valid("CS05") == False
+
+def test_punctuation():
+    assert is_valid("PT3.14") == False
+    assert is_valid("PT!314") == False
+    assert is_valid("PT 314") == False
 
 
 if __name__ == "__main__":
