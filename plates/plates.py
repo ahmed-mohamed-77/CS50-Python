7b0339518e74461f38026288028744548dad5423
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-01-26 08:10:19 +0000

    Fri, Jan 26, 2024, 10:10 AM +02:00

--- plates/plates.py
+++ plates/plates.py
@@ -12,7 +12,10 @@ def is_valid(plate_number):
     # min == 2
     if 2 > len(plate_number) > 6:
         return False
-    if isinstance(int, plate_number[2:5]) ==False and is
+
+    if isinstance(int, plate_number[2:5]) ==False and isinstance(int, plate_number[-1]) == True:
+        return False
+
     if plate_number[0].isalpha() == False or  plate_number[1].isalpha() == False:
         return False
     i = 0
