6d01cc3f6d37159d705b68fa4488550268596ed3
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-01-26 17:34:31 +0000

    Fri, Jan 26, 2024, 7:34 PM +02:00

--- plates/test_plates/test_plates.py
+++ plates/test_plates/test_plates.py
@@ -1,5 +1,21 @@
 from plates.plates import *
 
+def main():
+    test_is_valid()
+    test_check_lengh()
+
 def test_is_valid():
-    assert is_valid("H") == "Invalid"
+    assert is_valid("H") == False
+    assert is_valid("CS50") == True
+    assert is_valid("A2") == False
+
+def test_check_lengh():
+    assert check_length("A2") == True
+    assert check_length("cs") == True
+    assert check_length("2ANIPBIPRB") == False
+    assert check_length("CS50") == True
+
+
+if __name__ == "__main__":
+    main()
 
