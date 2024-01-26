b0034400d21853b3b4ee12ee57326dfe976fcc4e
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-01-26 08:44:13 +0000

    Fri, Jan 26, 2024, 10:44 AM +02:00

--- plates/plates.py
+++ plates/plates.py
@@ -49,5 +49,5 @@ def check_numbers(s):
             return False
     return True
 
-
-main()
+if __name__ == "__main__":
+    main()
---
+++ plates/test_plates/test_plates.py
@@ -0,0 +1,4 @@
+from plates.plates import is_valid
+
+def test_is_valid():
+    assert is_valid("H") == "Invalid"
