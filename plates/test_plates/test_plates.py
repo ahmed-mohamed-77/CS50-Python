d6556ef31364868d4860a11363f92591c8f9dba1
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-01-27 15:26:57 +0000

    Sat, Jan 27, 2024, 5:26 PM +02:00

--- plates/test_plates/test_plates.py
+++ plates/test_plates/test_plates.py
@@ -1,29 +1,27 @@
 from plates.plates import *
 
 
-def test_is_valid():
-    assert is_valid("CS50") == True
-    assert is_valid("ABCDEF") == True
+def test_length():
+    # maximum of 6 and minimum of 2 characters
+    assert is_valid("WXCS2976") == False
+    assert is_valid("WX") == True
+    assert is_valid("KY7689") == True
+
+def test_firstTwoCharacter():
+    # must start with at least 2 letters
+    assert is_valid("TY") == True
+    assert is_valid("F5") == False
+    assert is_valid("2976") ==  False
+
+def test_symbols():
+    # No periods, spaces, or punctuation marks are allowed
+    assert is_valid("WXCS2#") == False
+    assert is_valid("J@KOL!") == False
+
+def test_format():
+    # Numbers cannot be used in the middle of a plate; they must come at the end.
+    # For example, AAA222 would be an acceptable … vanity plate; AAA22A would not be acceptable.
+    # The first number used cannot be a '0'
     assert is_valid("AAA222") == True
     assert is_valid("AAA22A") == False
-
-
-def test_check_lengh():
-    assert is_valid("A2") == False
-    assert is_valid("AA") == True
-    assert is_valid("2A") == False
-    assert is_valid("22") == False
-
-def test_min_max_character():
-    assert is_valid("ABCDEFGH") == False
-    assert is_valid("H") == False
-    assert is_valid("A") == False
-
-def test_num_zero():
-    assert is_valid("CS50") == True
-    assert is_valid("CS05") == False
-
-def test_punctuation():
-    assert is_valid("PT3.14") == False
-    assert is_valid("PT!314") == False
-    assert is_valid("PT 314") == False
+    assert is_valid("AAA022") == False
