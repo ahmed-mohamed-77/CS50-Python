2c0e542d21914e08ed39b0020113eb736af19410
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-01-29 23:52:19 +0000

    Tue, Jan 30, 2024, 1:52 AM +02:00

---
+++ pizza/pizza.py
@@ -0,0 +1,23 @@
+import csv
+import sys
+import tabulate
+
+if len(sys.argv) == 1:
+    sys.exit("Too few command-line arguments")
+elif len(sys.argv) == 2:
+    try:
+        if not sys.argv[1].endswith(".csv"):
+            sys.exit("Not a CSV file")
+
+        with open(sys.argv[1], "r") as file:
+            reader = csv.reader(file)
+            data = [line for line in reader]
+
+        if data:
+            header = data[0]
+            table = tabulate.tabulate(data[1:],headers=header,tablefmt="grid")
+            print(table)
+        else:
+            print("no data inside the table".title())
+    except FileNotFoundError:
+        sys.exit("File does not exist")
