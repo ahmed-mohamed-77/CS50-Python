8ec4786e8662e8a0b921ce0e46ae0c53f4e482e0
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-01-30 14:30:49 +0000

    Tue, Jan 30, 2024, 4:30 PM +02:00

---
+++ scourgify/scourgify.py
@@ -0,0 +1,53 @@
+import sys
+import csv
+
+
+def main():
+    check_commandline_argument()
+
+    output = []
+    try:
+        with open(sys.argv[1], "r") as file_reader:
+
+            reader = csv.DictReader(file_reader)
+
+            for row in reader:
+                first_name, last_name = row["name"].split(",")
+                output.append(
+                    {
+                        "first": first_name.strip(),
+                        "last": last_name.strip(),
+                        "house": row["house"].strip(),
+                    }
+                )
+
+    except FileNotFoundError:
+        sys.exit(f"Could not read {sys.argv[1]}")
+
+    with open(sys.argv[2], "w") as file_writer:
+        writer = csv.DictWriter(file_writer, fieldnames=["first", "last", "house"])
+        writer.writerow({"first": "first", "last": "last", "house": "house"})
+
+        for row in output:
+            writer.writerow(
+                {"first": row["first"], "last": row["last"], "house": row["house"]}
+            )
+
+
+def check_commandline_argument():
+
+    if len(sys.argv) == 1:
+        sys.exit("Too few command-line arguments")
+
+    elif len(sys.argv) == 2:
+        sys.exit("Too few command-line arguments")
+
+    elif len(sys.argv) > 3:
+        sys.exit("Too many command-line arguments")
+
+    if not sys.argv[1].endswith(".csv") or not sys.argv[2].endswith(".csv"):
+        sys.exit("Not a CSV file")
+
+
+if __name__ == "__main__":
+    main()
