d2012ec489b52a4b0b54673dcea2684454ba1251
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-02-02 10:10:21 +0000

    Fri, Feb 2, 2024, 12:10 PM +02:00

--- watch/watch.py
+++ watch/watch.py
@@ -9,12 +9,12 @@ def main():
 def parse(s):
     pattern_one = r"<iframe.*?src=\"(https?://(?:www)?\.?\w+\.\w+(?:\.\w+)?/\w+(\/[a-zA-Z0-9?=-]+)?)\""
     if check := re.search(pattern_one, s):
-        search_part = f"https://youtu.be{check.group(2)}"
-        return search_part
-    
+        youtube_search = re.search(r"<iframe.*?src=\"(https?://(?:www\.?)?youtube\.\w+(?:\.\w+)?/\w+(\/[a-zA-Z0-9?=-]+)?)\"",s)
+        if not youtube_search:
+            return None
+        else:
+            search_part = f"https://youtu.be{check.group(2)}"
+            return search_part
 
 if __name__ == "__main__":
     main()
-
-
-
