c8ec9994265792d00ef0225639b559915b7f2835
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-02-03 10:57:55 +0000

    Sat, Feb 3, 2024, 12:57 PM +02:00

---
+++ um/test_um.py
@@ -0,0 +1,15 @@
+from project_testing.source.um import count
+
+
+def test_upper_lower_case():
+    assert count("Um, thanks for the album.") == 1
+    assert count("Um, thanks, um...") == 2
+
+
+def test_word_with_um():
+    assert count("yummy") == 0
+
+
+def test_surrounded_by_space():
+    assert count("hello, um, world") == 1
+    assert count("hello, um?") == 1
