a3a897dd58a83b51c4b6eddea663a3915e8f22d1
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-02-04 12:19:20 +0000

    Sun, Feb 4, 2024, 2:19 PM +02:00

--- bank/bank.py
+++ bank/bank.py
@@ -1,15 +1,12 @@
 def main():
-    print(value(input("Greeting: ").strip().lower()))
+    greeting_customer = value(input("Greeting: ").strip().lower())
+    print(f"${greeting_customer}")
 
 
 def value(greeting):
     if greeting.startswith("hello"):
-        return "$0"
+        return 0
     elif greeting.startswith("h"):
-        return "$20"
+        return 20
     else:
-        return "$100"
-
-
-if __name__ == "__main__":
-    main()
+        return 100
