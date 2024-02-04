437f8fa7a501c6675b2d39ff4d7f583c2f4983aa
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-02-04 16:11:39 +0000

    Sun, Feb 4, 2024, 6:11 PM +02:00

--- numb3rs/numb3rs.py
+++ numb3rs/numb3rs.py
@@ -6,7 +6,9 @@ def main():
 
 
 def validate(ip):
-    if ip_address := re.search(r"([0-9]+)\.([0-9]+)\.([0-9]+)\.([0-9]+)", ip):
+    if ip_address := re.search(r"([0-9]+)\.([0-9]+)\.([0-9]+)\.([0-9]+)(?:\.)?([0-9]+)?", ip):
+        if len(ip_address.groups()) > 4:
+            return False
         section_one, section_two, section_three, host = map(int, ip_address.groups())
         if section_one <= 255 and section_two <= 255 and section_three <= 255 and host <= 255:
             if section_one == 0 and section_two == 0 and section_three == 0 and host == 0:
