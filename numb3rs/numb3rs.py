2498ad5bd77be21f79ec72fdb01578d041cc5f82
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-02-04 15:36:44 +0000

    Sun, Feb 4, 2024, 5:36 PM +02:00

---
+++ numb3rs/numb3rs.py
@@ -0,0 +1,21 @@
+import re
+
+
+def main():
+    print(validate(input("IPv4 Address: ")))
+
+
+def validate(ip):
+    if ip_address := re.search(r"([0-9]+)\.([0-9]+)\.([0-9]+)\.([0-9]+)", ip):
+        section_one, section_two, section_three, host = map(int, ip_address.groups())
+        if section_one <= 255 and section_two <= 255 and section_three <= 255 and host <= 255:
+            if section_one == 0 and section_two == 0 and section_three == 0 and host == 0:
+                return False
+            return True
+    else:
+        return False
+
+
+if __name__ == "__main__":
+    main()
+    
