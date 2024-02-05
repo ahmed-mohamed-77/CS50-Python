94eda8334c211df91d0c16dea9c09041c4132e7b
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-02-05 17:29:13 +0000

    Mon, Feb 5, 2024, 7:29 PM +02:00

--- numb3rs/numb3rs.py
+++ numb3rs/numb3rs.py
@@ -11,24 +11,9 @@ def validate(ip):
         r"0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b"
     )
     if ip_address := re.fullmatch(pattern, ip):
-        ip_net_one, ip_net_two, ip_net_three, ip_net_host = map(
-            int, ip_address.groups()
-        )
-        if (
-            0 <= ip_net_one <= 255
-            and 0 <= ip_net_two <= 255
-            and 0 <= ip_net_three <= 255
-            and 0 <= ip_net_host <= 255
-        ):
-            return True
-        if (
-            ip_net_one == 0
-            and ip_net_two == 0
-            and ip_net_three == 0
-            and ip_net_host == 0
-        ):
-            return False
-    return False
+        return True
+    else:
+        return False
 
 
 if __name__ == "__main__":
