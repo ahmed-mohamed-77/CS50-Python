cf6817d78f73fb0f01d6b0fe5019967879fb997f
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-02-05 16:39:39 +0000

    Mon, Feb 5, 2024, 6:39 PM +02:00

--- numb3rs/numb3rs.py
+++ numb3rs/numb3rs.py
@@ -8,7 +8,7 @@ def main():
 def validate(ip):
     pattern = r"\b(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b"
 
-    if ip_address := re.fullmatch(pattern, ip):
+    if ip_address := re.search(pattern, ip):
         return True
     else:
         return False
