a69e26811d947325e764e9019942098e7c96d38a
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-02-05 17:03:49 +0000

    Mon, Feb 5, 2024, 7:03 PM +02:00

--- numb3rs/test_numb3rs.py
+++ numb3rs/test_numb3rs.py
@@ -2,12 +2,12 @@ from numb3rs import validate
 
 
 def test_validate():
-    assert validate("127.0.0.1") == True
-    assert validate("255.255.255.255") == True
-    assert validate("512.512.512.512") == False
-    assert validate("1.2.3.1000") == False
+    assert validate("127.0.0.1") is True
+    assert validate("255.255.255.255") is True
+    assert validate("512.512.512.512") is False
+    assert validate("1.2.3.1000") is False
 
 
 def test_false_ip():
-    assert validate("cat") == False
-    assert validate("10.10.10.10.10") == False
+    assert validate("cat") is False
+    assert validate("10.10.10.10.10") is False
