ce4289360bf7fd6caba10604cd7fd680830fbe74
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-02-05 16:58:39 +0000

    Mon, Feb 5, 2024, 6:58 PM +02:00

--- numb3rs/numb3rs.py
+++ numb3rs/numb3rs.py
@@ -6,12 +6,31 @@ def main():
 
 
 def validate(ip):
-    pattern = r"\b(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b"
-
-    if ip_address := re.search(pattern, ip):
-        return True
+    pattern = (
+        r"\b(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2["
+        r"0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b"
+    )
+    if ip_address := re.fullmatch(pattern, ip):
+        ip_net_one, ip_net_two, ip_net_three, ip_net_host = map(
+            int, ip_address.groups()
+        )
+        if (
+            0 <= ip_net_one <= 255
+            and 0 <= ip_net_two <= 255
+            and 0 <= ip_net_three <= 255
+            and 0 <= ip_net_host <= 255
+        ):
+            return True
+        if (
+            ip_net_one == 0
+            and ip_net_two == 0
+            and ip_net_three == 0
+            and ip_net_host == 0
+        ):
+            return False
     else:
         return False
+
 
 if __name__ == "__main__":
     main()
