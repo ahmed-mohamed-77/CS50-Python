a5e9ae1a73d20b93d27087f75414af571c216f5e
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-02-09 18:06:33 +0000

    Fri, Feb 9, 2024, 8:06 PM +02:00

--- jar/jar.py
+++ jar/jar.py
@@ -1,5 +1,7 @@
+# Define the Jar class
 class Jar:
     def __init__(self, capacity=12):
+        self._cookies = 0
         self.capacity = capacity
 
     @property
@@ -9,7 +11,7 @@ class Jar:
     @capacity.setter
     def capacity(self, capacity):
         if not isinstance(capacity, int) or capacity < 0:
-            raise ValueError
+            raise ValueError("Capacity must be a non-negative integer.")
         self._capacity = capacity
 
     @property
@@ -21,14 +23,17 @@ class Jar:
 
     def deposit(self, n) -> None:
         if not isinstance(n, int) or n < 0:
-            raise ValueError
+            raise ValueError("Number of cookies must be a non-negative integer.")
         if self._cookies + n > self._capacity:
-            raise ValueError
+            raise ValueError("Adding cookies would exceed the jar's capacity.")
         self._cookies += n
 
     def withdraw(self, n) -> None:
         if not isinstance(n, int) or n < 0:
-            raise ValueError
+            raise ValueError("Number of cookies must be a non-negative integer.")
         if self._cookies - n < 0:
-            raise ValueError
+            raise ValueError("Not enough cookies in the jar to withdraw.")
         self._cookies -= n
+
+
+
