8913a80efa4e227e21d51098cdb5feda7d081878
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-02-10 13:01:43 +0000

    Sat, Feb 10, 2024, 3:01 PM +02:00

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -4,22 +4,23 @@ import sys
 
 
 def main():
-    try:
         p = inflect.engine()
         age = input("Date of Birth: ").strip()
-        user_age = datetime.strptime(age, "%Y-%m-%d").date()
-        if not user_age:
-            raise ValueError
+
+        try:
+            user_age = datetime.strptime(age, "%Y-%m-%d").date()
+            if not user_age:
+                raise ValueError
+        except ValueError:
+            sys.exit("Invalid date")
 
         today_date = datetime.today().date()
         count_leapyear = count_leap_year(user_age.year, today_date.year)
 
-        user_age_in_days = (today_date - user_age).days
-        user_age_in_minutes = (user_age_in_days * 24 * 60)
+        user_age_in_days = today_date - user_age
+        user_age_in_minutes = user_age_in_days * 24 * 60
         convert_number_to_word = p.number_to_words(user_age_in_minutes)
         print(f"{convert_number_to_word} minutes")
-    except ValueError:
-        sys.exit("Invalid date")
 
 
 def count_leap_year(start_date, end_date) -> int:
@@ -36,4 +37,3 @@ def is_leap_year(year) -> bool:
 
 if __name__ == "__main__":
     main()
-
