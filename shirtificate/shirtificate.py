2ef095ff93403d28a7a162ea50d95f758fd5c25b
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-02-10 17:18:13 +0000

    Sat, Feb 10, 2024, 7:18 PM +02:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -6,7 +6,7 @@ class PDF:
         self._pdf.add_page()
         self._pdf.set_font("helvetica", "B", 16)
         self._pdf.cell(0,60,"CS50 Shirtificate", new_x="LMARGIN", new_y="NEXT", align="C")
-        self._pdf.image(r"/home/ubuntu/CS50-Python/shirtificate/shirtificate.png", w=self._pdf.epw)
+        self._pdf.image("shirtificate.png", w=self._pdf.epw)
         self._pdf.set_font_size(30)
         self._pdf.set_text_color(255,255,255)
         self._pdf.text(x =47.5, y=140, text=f"{name} took CS50")
