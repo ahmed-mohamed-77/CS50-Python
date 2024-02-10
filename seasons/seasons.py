33f75475b963771e1f363c6d6eb9515667054745
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-02-10 13:15:06 +0000

    Sat, Feb 10, 2024, 3:15 PM +02:00

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -4,23 +4,28 @@ import sys
 
 
 def main():
-        p = inflect.engine()
-        age = input("Date of Birth: ").strip()
+    p = inflect.engine()
+    age = input("Date of Birth: ").strip()
 
-        try:
-            user_age = datetime.strptime(age, "%Y-%m-%d").date()
-            if not user_age:
-                raise ValueError
-        except ValueError:
-            sys.exit("Invalid date")
+    try:
+        user_age = datetime.strptime(age, "%Y-%m-%d").date()
+        if not user_age:
+            raise ValueError
+    except ValueError:
+        sys.exit("Invalid date")
 
-        today_date = datetime.today().date()
-        count_leapyear = count_leap_year(user_age.year, today_date.year)
+    today_date = datetime.today().date()
+    count_leapyear = count_leap_year(user_age.year, today_date.year)
 
-        user_age_in_days = today_date - user_age
-        user_age_in_minutes = user_age_in_days * 24 * 60
-        convert_number_to_word = p.number_to_words(user_age_in_minutes)
-        print(f"{convert_number_to_word} minutes")
+    user_age_in_days = (today_date - user_age).days
+
+
+    # count days in minutes
+    user_age_in_minutes = user_age_in_days * 24 * 60
+
+
+    convert_number_to_word = p.number_to_words(user_age_in_minutes, andword="")
+    print(f"{convert_number_to_word.capitalize()} minutes")
 
 
 def count_leap_year(start_date, end_date) -> int:
