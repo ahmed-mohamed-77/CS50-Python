541a23d019afbf40e413567cbd6fd5949a8ae936
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-02-10 12:42:01 +0000

    Sat, Feb 10, 2024, 2:42 PM +02:00

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -15,12 +15,11 @@ def main():
         count_leapyear = count_leap_year(user_age.year, today_date.year)
 
         user_age_in_days = (today_date - user_age).days
-        user_age_in_minutes = (user_age_in_days * 24 * 60) + (count_leapyear * 24 * 60)
+        user_age_in_minutes = (user_age_in_days * 60) + (count_leapyear * 60)
         convert_number_to_word = p.number_to_words(user_age_in_minutes)
         print(f"{convert_number_to_word} minutes")
     except ValueError:
         sys.exit("Invalid date")
-
 
 
 def count_leap_year(start_date, end_date) -> int:
