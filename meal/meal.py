a7a6d9f3132d97648e6ee49990c0d0f5606a538d
Ahmed Ghoniem <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-09-23 17:28:18 +0000

    Mon, Sep 23, 2024, 8:28 PM +03:00

--- meal/meal.py
+++ meal/meal.py
@@ -1,17 +1,32 @@
+# target from this function is to return the time in decimal form
+# like 7.5 ,  8.0 , 9.1
+def convert(time: str) -> float:
+    hours, minutes = map(int, time.split(":"))
+    return hours + minutes / 60
+    # if it decimal value
+
+
+
 def main():
-    time_str = input("Enter the time in 24-hour format (HH:MM): ")
-    hour = convert(time_str)
+    # Check the format is ver important
+    user_input: str = input("What time is it format (HH:MM): ").strip()
 
-    if 7.0 <= hour <= 8.0:
-        print("breakfast time")
-    elif 12.0 <= hour <= 13.0:
-        print("lunch time")
-    elif 18.0 <= hour <= 19.0:
-        print("dinner time")
+    try:
+        time: float = convert(time=user_input)
 
-def convert(time_str):
-    hours, minutes = map(int, time_str.split(":"))
-    return hours + minutes / 60.0
+        """
+        breakfast between 7:00 and 8:00, lunch between 12:00 and 13:00,
+        and dinner between 18:00 and 19:00.
+        """
+        if 7.0 <= time <= 8.0:
+            print("breakfast time")
+        elif 12.0 <= time < 13.0:
+            print("lunch time")
+        elif 18.0 <= time < 19.5:
+            print("dinner time")
+    except ValueError:
+        print("Invalid time  Format".upper())
+
 
 if __name__ == "__main__":
     main()
