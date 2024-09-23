c8ed67553b5fad09e2c55d0d8c07848624d5b0d4
Ahmed Ghoniem <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-09-23 17:02:28 +0000

    Mon, Sep 23, 2024, 8:02 PM +03:00

--- meal/meal.py
+++ meal/meal.py
@@ -11,6 +11,7 @@ def main():
 
 def convert(time_str):
     hours, minutes = map(int, time_str.split(":"))
+    print(hours + minutes / 60.0)
     return hours + minutes / 60.0
 
 if __name__ == "__main__":
